20a133e3f574b3b3a8bc2d992a6d1bfa40899860
thanhnt <thanhnt.k56@gmail.com>
2015-12-14 08:53:43 +0700

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+if [ $# -lt 1 ]; then
+echo "
+Usage:
+    ./build.sh [0|1]
+        0: don't do cross compile for arm
+        1: do cross compile for arm
+"
+exit
+fi
 #define constant variable
 LINUX_ARMV7L=$PWD/../libs/linux-armv7l
 LINUX_X86_64=$PWD/../libs/linux-x86_64
@@ -6,6 +15,7 @@ LINUX_I686=$PWD/../libs/linux-i686
 MINGW=$PWD/../libs/mingw32-i586
 MACOS=$PWD/../libs/darwin-x86_64
 MSYS=$PWD/../libs/msys-i686
+ARM=$1
 #MACOS
 uname -a | grep "Darwin"
 if [ $? == 0 ]; then
@@ -35,14 +45,16 @@ if [ $? == 0 ]; then
 	fi
 fi
 
-#build for arm first
-rm -rf $LINUX_ARMV7L/include/json-c
-rm -rf $LINUX_ARMV7L/lib/libjson-c.*
-make distclean
-./autogen.sh
-./configure --host=arm-linux-gnueabi --enable-shared=no --enable-static --prefix=$LINUX_ARMV7L
-make
-make install
+if [ $ARM == 1 ]; then
+	#build for arm first
+	rm -rf $LINUX_ARMV7L/include/json-c
+	rm -rf $LINUX_ARMV7L/lib/libjson-c.*
+	make distclean
+	./autogen.sh
+	./configure --host=arm-linux-gnueabi --enable-shared=no --enable-static --prefix=$LINUX_ARMV7L
+	make
+	make install
+fi
 
 #build for intel
 rm -rf $INSTALL_DIR/include/json-c
